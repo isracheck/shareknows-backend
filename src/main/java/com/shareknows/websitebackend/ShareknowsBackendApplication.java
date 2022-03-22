4ce8ebd9247b23e252e964cc1a39340a3b738a0a
“isracheck” <israelgutierrez@outlook.com>
2022-03-22 23:47:16 +0100

    first commit

---
+++ src/main/java/com/shareknows/websitebackend/ShareknowsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shareknows.websitebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareknowsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareknowsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shareknows/websitebackend/ShareknowsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shareknows.websitebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareknowsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
